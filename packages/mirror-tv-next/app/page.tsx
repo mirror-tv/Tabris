d42f040ee712fe29d5d4e47846c7d715cbd1ab43
YiHsuan <v61265@gmail.com>
2024-11-07 15:56:27 +0800

    refactor: move getFeatureTopics into TopicList

--- packages/mirror-tv-next/app/page.tsx
+++ packages/mirror-tv-next/app/page.tsx
@@ -7,50 +7,12 @@ import GptPopup from '~/components/ads/gpt/gpt-popup'
 import { GLOBAL_CACHE_SETTING } from '~/constants/environment-variables'
 import PopularPostsList from '~/components/homepage/popular-posts-list'
 import TopicList from '~/components/homepage/topic-list'
-import { type FeatureTopic } from '~/graphql/query/topic'
-import errors from '@twreporter/errors'
-import { handleResponse } from '~/utils'
-import { getFeatureTopics } from './_actions/homepage/feature-topics'
 
 const GPTAd = dynamic(() => import('~/components/ads/gpt/gpt-ad'))
 
 export const revalidate = GLOBAL_CACHE_SETTING
 
-export default async function Home() {
-  let allTopics: FeatureTopic[] = []
-
-  try {
-    const responses = await Promise.allSettled([
-      getFeatureTopics({ topicFirst: 4, postFirst: 3 }),
-    ])
-    allTopics = handleResponse(
-      responses[0],
-      (
-        featureTopicsData:
-          | Awaited<ReturnType<typeof getFeatureTopics>>
-          | undefined
-      ) => {
-        return featureTopicsData?.data?.allTopics ?? []
-      },
-      'Error occurs while fetching feature topics data in homepage'
-    )
-  } catch (error) {
-    console.error(
-      JSON.stringify({
-        severity: 'ERROR',
-        message: errors.helpers.printAll(
-          error,
-          {
-            withStack: true,
-            withPayload: true,
-          },
-          0,
-          0
-        ),
-      })
-    )
-  }
-
+export default function Home() {
   return (
     <main className={styles.main}>
       <GptPopup adKey="MB_HOME" />
@@ -67,7 +29,7 @@ export default async function Home() {
         <MainFlashNews />
       </div>
       <PopularPostsList title="熱門新聞" />
-      <TopicList title="推薦專題" topics={allTopics} />
+      <TopicList title="推薦專題" />
     </main>
   )
 }
--- packages/mirror-tv-next/components/homepage/topic-list.tsx
+++ packages/mirror-tv-next/components/homepage/topic-list.tsx
@@ -2,22 +2,45 @@ import { type FeatureTopic } from '~/graphql/query/topic'
 import styles from './_styles/topic-list.module.scss'
 import UiHeadingBordered from '~/components/shared/ui-heading-bordered'
 import TopicItem from './topic-item'
+import { getFeatureTopics } from '~/app/_actions/homepage/feature-topics'
+import errors from '@twreporter/errors'
 
 type TopicListProps = {
   title: string
-  topics: FeatureTopic[]
 }
 
-export default function TopicList({ title, topics = [] }: TopicListProps) {
+export default async function TopicList({ title }: TopicListProps) {
+  let allTopics: FeatureTopic[] = []
+
+  try {
+    const response = await getFeatureTopics({ topicFirst: 4, postFirst: 3 })
+    allTopics = response?.data?.allTopics
+  } catch (error) {
+    console.error(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(
+          error,
+          {
+            withStack: true,
+            withPayload: true,
+          },
+          0,
+          0
+        ),
+      })
+    )
+  }
+
   return (
     <section className={styles.container}>
       <div className={styles.titleWrapper}>
         <UiHeadingBordered title={title} className={styles.listTitle} />
       </div>
       <div>
-        <TopicItem isFirst={true} topic={topics[0]} />
+        <TopicItem isFirst={true} topic={allTopics[0]} />
         <div className={styles.list}>
-          {topics.slice(1).map((topic) => {
+          {allTopics.slice(1).map((topic) => {
             return <TopicItem isFirst={false} topic={topic} key={topic.slug} />
           })}
         </div>
