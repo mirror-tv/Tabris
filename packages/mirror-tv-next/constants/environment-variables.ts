1f19a36d864c926907e96fa3797c410e0ac195e5
YiHsuan <v61265@gmail.com>
2025-07-18 13:22:37 +0800

    refactor: add console to get type

--- packages/mirror-tv-next/constants/environment-variables.ts
+++ packages/mirror-tv-next/constants/environment-variables.ts
@@ -1,6 +1,6 @@
 // 這裡管理的是在 Build 階段就會寫死數值的環境變數 (通常為 `NEXT_PUBLCI_` 開頭)
 const ENV = process.env.NEXT_PUBLIC_ENV || 'local'
-const RECALL_FEATURE_TOGGLE_2025 = process.env.NEXT_PUBLIC_SPECIALEVENT || false
+const RECALL_FEATURE_TOGGLE_2025 = process.env.NEXT_PUBLIC_SPECIALEVENT
 let SITE_URL: string
 let GTM_ID: string
 let GLOBAL_CACHE_SETTING: number
