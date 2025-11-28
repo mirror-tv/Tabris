26a41c8b2b8dd6eb1967a1d3e982a5b94cfbc01b
YiHsuan <v61265@gmail.com>
2025-11-28 15:06:32 +0800

    refactor(ad/tranferr-sender): adjust html of email content

--- packages/tv-ad-admin-next/utils/transferr-sender.ts
+++ packages/tv-ad-admin-next/utils/transferr-sender.ts
@@ -25,33 +25,33 @@ async function sendTransferEmailWithTemplate(
     body = `
 <h2>訂單${originalOrderNumber}已轉移至新訂單${newOrderNumber}</h2>
 
-親愛的 ${memberName}，您好：
-您的訂單修改需求系統已收到，新訂單編號：${newOrderNumber}。
+<p>親愛的 ${memberName}，您好：</p>
+<p>您的訂單修改需求系統已收到，新訂單編號：${newOrderNumber}。</p> 
 
+<ul>
+<li>訂單編號：${newOrderNumber}</li>
+<li>廣告名稱：${newOrderName}</li>
+</ul>
+<p>此為系統自動通知信件，請勿回覆此郵件，如需要聯繫客服，請寫信至 mnews_sales@mnews.tw</p>
 
-- 訂單編號：${newOrderNumber}
-- 廣告名稱：${newOrderName}
-
-此為系統自動通知信件，請勿回覆此郵件，如需要聯繫客服，請寫信至 mnews_sales@mnews.tw
-
-鏡電視廣告團隊
+<p>鏡電視廣告團隊</p>
     `
   } else {
     subject = `訂單${originalOrderNumber}已轉移至新訂單${newOrderNumber}`
     body = `
 <h2>用戶已完成訂單關聯，並重新上傳需修改素材</h2>
 
-您好，
-用戶已完成需修改素材上傳，請盡快至CMS確認素材。
+<p>您好，</p>
+<p>用戶已完成需修改素材上傳，請盡快至CMS確認素材。</p>
 
+<ul>
+<li>訂單編號：${newOrderNumber}</li>
+<li>廣告名稱：${newOrderName}</li>
+<li>會員：${memberName}</li></ul>
 
-- 訂單編號：${newOrderNumber}
-- 廣告名稱：${newOrderName}
-- 會員：${memberName}
+<p>此為系統自動通知信件，請勿回覆此郵件。</p>
 
-此為系統自動通知信件，請勿回覆此郵件。
-
-鏡電視廣告系統
+<p>鏡電視廣告系統</p>
     `
   }
 
