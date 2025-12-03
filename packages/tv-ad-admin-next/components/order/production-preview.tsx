78ae8c05c9cfc906de4c0a32613b6c27a2e54f08
YiHsuan <v61265@gmail.com>
2025-12-04 07:44:54 +0800

    feat(order): support multiple demo images in production preview

--- packages/tv-ad-admin-next/components/order/production-preview.tsx
+++ packages/tv-ad-admin-next/components/order/production-preview.tsx
@@ -1,6 +1,9 @@
+import { useMemo } from 'react'
+
 import Image from 'next/image'
 
 import { OrderRecordForOrderNumber } from '@/graphql/queries/orders'
+
 type ProductionPreviewProps = {
   className?: string
   order: OrderRecordForOrderNumber
@@ -10,6 +13,15 @@ export function ProductionPreview({
   className = '',
   order,
 }: ProductionPreviewProps) {
+  const demoImages = useMemo(
+    () =>
+      Array.isArray(order.demoImage)
+        ? order.demoImage
+        : order.demoImage
+          ? [order.demoImage]
+          : null,
+    [order.demoImage]
+  )
   return (
     <div className={className}>
       <h5 className="mb-4 text-text-primary">製作成品預覽</h5>
@@ -17,18 +29,27 @@ export function ProductionPreview({
         <h6 className="font-sans text-sm leading-normal font-medium text-text-secondary">
           影片截圖
         </h6>
-        <div className="relative my-2 aspect-video w-full overflow-hidden rounded-lg bg-gray-2">
-          {order.demoImage?.url ? (
-            <Image
-              src={order.demoImage.url}
-              alt="video-preview"
-              fill
-              className="object-cover"
-              loading="lazy"
-            />
+        <div className="flex flex-col gap-1">
+          {demoImages?.[0]?.url ? (
+            demoImages.map((image) => (
+              <div
+                className="relative my-2 aspect-video w-full overflow-hidden rounded-lg bg-gray-1"
+                key={image.id}
+              >
+                <Image
+                  src={image.url || ''}
+                  alt="video-preview"
+                  fill
+                  className="object-cover"
+                  loading="lazy"
+                />
+              </div>
+            ))
           ) : (
-            <div className="flex h-full w-full items-center justify-center text-text-tertiary">
-              影片預覽圖還未上傳
+            <div className="relative my-2 flex aspect-video w-full flex-col gap-2 overflow-hidden rounded-lg bg-gray-1">
+              <div className="flex h-full w-full items-center justify-center text-text-tertiary">
+                影片預覽圖還未上傳
+              </div>
             </div>
           )}
         </div>
@@ -40,8 +61,8 @@ export function ProductionPreview({
           <div>
             <span>尺寸：</span>
             <span>
-              {order.demoImage?.imageFile?.width || '???'}x
-              {order.demoImage?.imageFile?.height || '???'}
+              {demoImages?.[0]?.imageFile?.width || '???'}x
+              {demoImages?.[0]?.imageFile?.height || '???'}
             </span>
           </div>
         </div>
