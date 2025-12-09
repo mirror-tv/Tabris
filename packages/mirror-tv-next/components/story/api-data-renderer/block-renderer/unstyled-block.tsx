f0e9d01b8c09617a7ea903a33ccb82b455259d84
YiHsuan <v61265@gmail.com>
2025-12-10 00:01:54 +0800

    refactor(tv-next/amp): add sanitizeAnchorTag

--- packages/mirror-tv-next/components/story/api-data-renderer/block-renderer/unstyled-block.tsx
+++ packages/mirror-tv-next/components/story/api-data-renderer/block-renderer/unstyled-block.tsx
@@ -1,22 +1,32 @@
 import styles from './_styles/unstyled-block.module.scss'
 import { type ApiDataBlockBase, ApiDataBlockType } from './type'
+import { sanitizeHtmlForAmp } from '~/utils/amp'
+import { getFirstElement } from '~/utils/common'
 
 export interface ApiDataUnstyled extends ApiDataBlockBase {
   type: ApiDataBlockType.Unstyled
-  content: string
+  content: string | string[]
   alignment: 'center'
 }
 const UnstyledBlock = ({
   data,
   className,
+  isAmp,
 }: {
   data: ApiDataUnstyled
   className?: string
+  isAmp?: boolean
 }) => {
+  // 處理 content 可能是數組的情況
+  const rawContent = Array.isArray(data.content)
+    ? getFirstElement(data.content)
+    : data.content
+  const content = isAmp ? sanitizeHtmlForAmp(rawContent) : rawContent
+
   return (
     <p
       className={`${styles.textBlock} ${className}`}
-      dangerouslySetInnerHTML={{ __html: data.content }}
+      dangerouslySetInnerHTML={{ __html: content }}
     />
   )
 }
