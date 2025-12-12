7afec770df682a7d990031b207184d2244421d79
Wendy Hu <yunhu412@gmail.com>
2025-12-12 19:50:06 +0800

    feat(actions-logging): add func logPageView with page-view event type

---
+++ packages/mirror-tv-next/app/actions-logging.ts
@@ -0,0 +1,22 @@
+import { Logging } from '@google-cloud/logging'
+import { GCP_PROJECT_ID } from '~/constants/config'
+import { ENV } from '~/constants/environment-variables'
+
+export async function logPageView() {
+  const logging = new Logging({ projectId: GCP_PROJECT_ID })
+  const eventType = 'page-view'
+  const logName = `${GCP_PROJECT_ID}-${ENV}-web-${eventType}`
+  const log = logging.log(logName)
+
+  const metadata = {
+    resource: { type: 'global' },
+    severity: 'DEFAULT',
+    labels: {
+      eventType,
+    },
+  }
+
+  const entry = log.entry(metadata)
+
+  return log.write(entry)
+}
