d62b8a22f9c29926038c89710a9e2a9a6fe6c871
Wendy Hu <yunhu412@gmail.com>
2025-12-12 20:10:24 +0800

    feat(actions-logging): add metadta.labels.date

--- packages/mirror-tv-next/app/actions-logging.ts
+++ packages/mirror-tv-next/app/actions-logging.ts
@@ -1,18 +1,28 @@
 import { Logging } from '@google-cloud/logging'
 import { GCP_PROJECT_ID } from '~/constants/config'
 import { ENV } from '~/constants/environment-variables'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-tw'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 export async function logPageView() {
   const logging = new Logging({ projectId: GCP_PROJECT_ID })
   const eventType = 'page-view'
   const logName = `${GCP_PROJECT_ID}-${ENV}-web-${eventType}`
   const log = logging.log(logName)
+  const taipeiTime = dayjs().tz('Asia/Taipei')
+  const eventTriggeredDate = taipeiTime.format('YYYY/MM/DD')
 
   const metadata = {
     resource: { type: 'global' },
     severity: 'INFO',
     labels: {
       eventType,
+      date: eventTriggeredDate,
     },
   }
 
