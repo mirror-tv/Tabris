ed4cb6829ff8358a7456e518d82bb3f71e5f02bc
Wendy Hu <yunhu412@gmail.com>
2025-12-15 17:26:21 +0800

    feat(user-agent): import next/header module and get needed data

--- packages/mirror-tv-next/utils/user-agent.ts
+++ packages/mirror-tv-next/utils/user-agent.ts
@@ -1,1 +1,12 @@
 import 'server-only'
+import { headers } from 'next/headers'
+
+export function parseUserAgentInfo() {
+  const headersList = headers()
+  const userAgent = headersList.get('user-agent') || ''
+  const ipAddress =
+    headersList.get('x-forwarded-for') || headersList.get('remote-addr') || ''
+  const pathname = headersList.get('referer') || ''
+
+  return { userAgent, ipAddress, pathname }
+}
