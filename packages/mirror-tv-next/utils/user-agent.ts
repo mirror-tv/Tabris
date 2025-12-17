ef6bd7c08881063292f8a150c8dbb84b82f87f15
Wendy Hu <yunhu412@gmail.com>
2025-12-17 11:48:21 +0800

    feat(user-agent): add user agent related data

--- packages/mirror-tv-next/utils/user-agent.ts
+++ packages/mirror-tv-next/utils/user-agent.ts
@@ -1,12 +1,29 @@
 import 'server-only'
 import { headers } from 'next/headers'
+import { NextRequest, userAgent } from 'next/server'
 
-export function parseUserAgentInfo() {
+export function parseUserAgentInfo(request: NextRequest) {
   const headersList = headers()
-  const userAgent = headersList.get('user-agent') || ''
+  const userAgenInfo = headersList.get('user-agent') || ''
   const ipAddress =
     headersList.get('x-forwarded-for') || headersList.get('remote-addr') || ''
   const pathname = headersList.get('referer') || ''
 
-  return { userAgent, ipAddress, pathname }
+  const { browser, device, os } = userAgent(request)
+
+  const uaBrowser = {
+    name: browser.name || '',
+    version: browser.version || '',
+  }
+
+  const uaDevice = {
+    model: device.model || '',
+    vendor: device.vendor || '',
+  }
+
+  const uaOS = {
+    name: os.name || '',
+    version: os.version || '',
+  }
+  return { userAgenInfo, ipAddress, pathname, uaBrowser, uaDevice, uaOS }
 }
