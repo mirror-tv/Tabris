26b37159abcba83fcba063e24f9b04259dbcf353
Wendy Hu <yunhu412@gmail.com>
2026-04-28 18:01:00 +0800

    feat(editor-choices-swiper): set object-fit value to 'contain' if post slug contains 'sot'

--- packages/mirror-tv-next/components/homepage/editor-choices-swiper.tsx
+++ packages/mirror-tv-next/components/homepage/editor-choices-swiper.tsx
@@ -90,18 +90,34 @@ export default function EditorChoicesSwiper({
                     rel="noreferrer noopener"
                   >
                     {choice.exclusive && <UiExclusiveMark />}
-                    <Image
-                      loadingImage="/images/loading.svg"
-                      defaultImage="/images/image-default.jpg"
-                      images={images}
-                      imagesWebP={imagesWebP}
-                      alt={choice.name}
-                      rwd={{
-                        tablet: '100px',
-                        desktop: '1000px',
-                      }}
-                      priority={false}
-                    />
+                    {choice.slug.includes('sot') ? (
+                      <Image
+                        loadingImage="/images/loading.svg"
+                        defaultImage="/images/image-default.jpg"
+                        images={images}
+                        imagesWebP={imagesWebP}
+                        alt={choice.name}
+                        rwd={{
+                          tablet: '100px',
+                          desktop: '1000px',
+                        }}
+                        priority={false}
+                        objectFit="contain"
+                      />
+                    ) : (
+                      <Image
+                        loadingImage="/images/loading.svg"
+                        defaultImage="/images/image-default.jpg"
+                        images={images}
+                        imagesWebP={imagesWebP}
+                        alt={choice.name}
+                        rwd={{
+                          tablet: '100px',
+                          desktop: '1000px',
+                        }}
+                        priority={false}
+                      />
+                    )}
                     <a
                       className={`${styles.nameWrapper} GTM-editor-choices-link`}
                       href={
