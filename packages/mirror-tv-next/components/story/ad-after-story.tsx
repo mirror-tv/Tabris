669ed12c8615bf6857289a4b9647c313c928a14c
Michael Kuo <sxdcfv20002000@gmail.com>
2026-06-03 11:58:33 +0800

    style: update class name for popinRecommend component

--- packages/mirror-tv-next/components/story/ad-after-story.tsx
+++ packages/mirror-tv-next/components/story/ad-after-story.tsx
@@ -68,7 +68,10 @@ const AdAfterStory: React.FC = () => {
               title="每日精選"
               className={styles.dailySelection}
             />
-            <div id="_popIn_recommend" className={styles.popinRecommend} />
+            <div
+              id="_popIn_recommend"
+              className={`popin_recommend ${styles.popinRecommend}`}
+            />
           </>
         )}
       </LazyRenderWrapper>
